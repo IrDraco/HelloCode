d94962cdc1b29f802f40b43569e899025e0b440c
Ir <rinadraco@gmail.com>
2023-01-10 23:58:02 +0300

    Перерыв на сон

---
+++ Ex0005_If_Else_Operator/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+    Console.WriteLine ("Ура! Маша пришла!");
+}
+else{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
