b2a94f36af8961b4df0ec4b4d406dd4b0022c7f9
Ir <rinadraco@gmail.com>
2023-01-10 23:33:36 +0300

    Add Rendom

--- Ex0003_Sum/Program.cs
+++ Ex0003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int namberA = 38;
-int namberB = 5;
+﻿int namberA = new Random().Next(1, 10); // случайное целое число от 1 до 9
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1, 10);
+Console.WriteLine(namberB);
 int Result = namberA + namberB;
 Console.WriteLine (Result);
